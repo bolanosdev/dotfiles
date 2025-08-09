0cc7d0b59bd9fa23f947feeb557242388bc35530
Carlos Bolanos <cbolanos@cloudflare.com>
2025-08-08 17:32:37 -0700

    add nvim options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,6 +1,7 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+vim.opt.clipboard = "unnamedplus"
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
@@ -8,3 +9,8 @@ vim.opt.expandtab = true
 
 vim.g.have_nerd_font = false
 vim.g.codeium_enabled = false
+
+vim.o.foldmethod = "expr"
+vim.o.foldexpr = "nvim_treesitter#foldexpr()"
+vim.o.foldenable = true
+vim.o.foldlevel = 99
